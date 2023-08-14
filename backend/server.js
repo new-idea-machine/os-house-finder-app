0b909e9a2360bf9510bbf054f4eec881c9a19771
jmeng-sys <charlesmeng268@gmail.com>
2023-08-13 23:54:11 -0600

    created bare bones of the project

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
