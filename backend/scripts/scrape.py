fa6d6a7fe1a49dcfe530ffb955685ca656656ce4
NathanEne <nathan@enerson.ca>
2023-09-06 19:37:39 -0600

    Version 2 of Scraper uses website headers and pulls more information, still needs more data processing but a good start.

--- backend/scripts/scrape.py
+++ backend/scripts/scrape.py
@@ -7,22 +7,40 @@ import requests
 
 def process_url(url):
     i = 0
-    keys = ["Property Type", "Building Type", "Storeys", "Square Footage", "Community Name", "Subdivision Name", "Title", "Land Size", "Built in", "Annual Property Taxes", "Parking Type", "Time on REALTOR.ca"]
     headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:82.0) Gecko/20100101 Firefox/82.0' } 
+    
     try: 
         page = requests.get(url, allow_redirects=False, headers = headers ,timeout=10)
     except requests.exceptions.Timeout as err: 
         print(err)
-
+    
     soup = BeautifulSoup(page.content, 'html.parser')
-    results = soup.find(id="PropertySummary")
-    propertyDetailData = results.find_all("div", class_="propertyDetailsSectionContentValue")
+    summaryResult = soup.find(id="PropertySummary")
+    
+    propertyDetailData = summaryResult.find_all("div", class_="propertyDetailsSectionContentValue")
+    labels = summaryResult.find_all("div", class_ = "propertyDetailsSectionContentLabel")
     processedString = "{"
-    for info in propertyDetailData:
-        processedString = processedString+ "\""+keys[i] +"\": \""+ info.text.strip()+ "\", "
+    for (info, label) in zip(propertyDetailData,labels):
+        processedString = processedString+ "\""+label.text.strip() +"\": \""+ info.text.strip()+ "\", "
         i = i + 1
-    processedString = processedString[:-2]
-    processedString = processedString + "}"
+    
+    buildingResult = soup.find(id = "listingDetailsBuildingCon")
+    buildingDetailData = buildingResult.find_all("div", class_ ="propertyDetailsSectionContentValue")
+    labels = buildingResult.find_all("div", class_ = "propertyDetailsSectionContentLabel")
+    for (info,label) in zip(buildingDetailData,labels):
+        processedString = processedString+ "\""+label.text.strip() +"\": \""+ info.text.strip()+ "\", "
+        i = i + 1
+    
+    landResult = soup.find(id = "propertyDetailsLandSection")
+    landDetailData = landResult.find_all("div", class_ ="propertyDetailsSectionContentValue")
+    labels = landResult.find_all("div", class_ = "propertyDetailsSectionContentLabel")
+    for (info,label) in zip(landDetailData,labels):
+        processedString = processedString+ "\""+label.text.strip() +"\": \""+ info.text.strip()+ "\", "
+        i = i + 1
+
+    imageResult = soup.find(id = "heroImage")
+    processedString += "\"imageLink\": "+ imageResult['src']
+    processedString += "}"
     return processedString
 
 if __name__ == "__main__":
