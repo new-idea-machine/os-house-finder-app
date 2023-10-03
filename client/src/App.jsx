67bdc3e8ccde27e81b75eb71d682df810250e7c2
javascriptsucks <evix1988@live.cn>
2023-10-03 05:37:27 -0600

    remove files not in use

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,6 @@ import 'react-toastify/dist/ReactToastify.css';
 import Header from '@components/Header/Header.jsx';
 import Footer from '@components/Footer';
 import ContextWrapper from './ContextWrapper';
-import './App.css';
 
 function App() {
   return (
