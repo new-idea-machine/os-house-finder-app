3ce282ea389f9c9a31487bdeabfee8f471398ffb
javascriptsucks <evix1988@live.cn>
2023-10-03 00:40:50 -0600

    config basic react vite with typescript

---
+++ frontend_vite/vite.config.ts
@@ -0,0 +1,23 @@
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@lib': path.resolve(__dirname, './src/lib'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@store': path.resolve(__dirname, './src/store'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@constants': path.resolve(__dirname, './src/constants'),
+      '@app': path.resolve(__dirname, './src/app'),
+      '@services': path.resolve(__dirname, './src/services'),
+      '@features': path.resolve(__dirname, './src/features'),
+    },
+  },
+});
