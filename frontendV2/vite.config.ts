c7fad5c0a3848ecf7dc122e970b439e17bf57e72
javascriptsucks <evix1988@live.cn>
2023-10-03 20:48:59 -0600

    copy frontendv2 and backendv2 from typescript

--- frontendV2/vite.config.ts
+++ frontendV2/vite.config.ts
@@ -4,7 +4,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,16 +12,5 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/setupTests.ts'],
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@lib': path.resolve(__dirname, './src/lib'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@app': path.resolve(__dirname, './src/app'),
-      '@features': path.resolve(__dirname, './src/features'),
-      '@screens': path.resolve(__dirname, './src/screens'),
-    },
   },
 });
