3748186027778f2c3821761c77d153c30ca24f60
Yzma <10090913+Yzma@users.noreply.github.com>
2023-10-05 11:35:57 -0400

    Make ContextWrapper typesafe

--- frontendV2/src/ContextWrapper.tsx
+++ frontendV2/src/ContextWrapper.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { PropsWithChildren } from 'react';
 import store from './app/store';
 
-export default function ContextWrapper({ children }) {
+export default function ContextWrapper({ children }: PropsWithChildren) {
   return (
     <Provider store={store}>
       <BrowserRouter>{children}</BrowserRouter>
