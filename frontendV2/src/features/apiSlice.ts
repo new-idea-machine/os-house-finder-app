26207432c8b1da1db4ac9fea684d18eb603a75b2
Yzma <10090913+Yzma@users.noreply.github.com>
2023-10-05 12:13:11 -0400

    Cleanup type errors for apiSlice and authSlice

--- frontendV2/src/features/apiSlice.ts
+++ frontendV2/src/features/apiSlice.ts
@@ -1,8 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { BASE_URL } from '@/constants';
 
-import { BASE_URL } from '../constants';
-
-const baseQuery = fetchBaseQuery({ baseURL: BASE_URL });
+const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
 
 export const apiSlice = createApi({
   baseQuery,
--- frontendV2/src/features/authSlice.ts
+++ frontendV2/src/features/authSlice.ts
@@ -2,7 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   userInfo: localStorage.getItem('userInfo')
-    ? JSON.parse(localStorage.getItem('userInfo'))
+    ? JSON.parse(localStorage.getItem('userInfo') ?? '{}')
     : null,
 };
 
