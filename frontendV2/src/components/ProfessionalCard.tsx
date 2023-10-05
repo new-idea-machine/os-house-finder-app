1582ceab143ac7147dd0bc5d8eb8b57e0837129c
Yzma <10090913+Yzma@users.noreply.github.com>
2023-10-04 22:51:05 -0400

    Format ProfessionalCard component

--- frontendV2/src/components/ProfessionalCard.tsx
+++ frontendV2/src/components/ProfessionalCard.tsx
@@ -5,7 +5,7 @@ type ProfessionalCardProps = {
   image: string;
 };
 
-function ProfessionalCard({
+export default function ProfessionalCard({
   name,
   title,
   description,
@@ -14,11 +14,9 @@ function ProfessionalCard({
   return (
     <div className="px-5">
       <img className="mx-auto" src={image} width={200} alt="Shirt" />
-      <p className="text-center fw-bold pt-3">{name}</p>
-      <p className="text-center fw-bold">{title}</p>
-      <p className="text-center fw-bold">{description}</p>
+      <p className="fw-bold pt-3 text-center">{name}</p>
+      <p className="fw-bold text-center">{title}</p>
+      <p className="fw-bold text-center">{description}</p>
     </div>
   );
 }
-
-export default ProfessionalCard;
