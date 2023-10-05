1dabc6db8b3ea52fb51d5bf07367455104b80c65
Yzma <10090913+Yzma@users.noreply.github.com>
2023-10-04 22:44:12 -0400

    Convert Footer component to tailwind

--- frontendV2/src/components/Footer.tsx
+++ frontendV2/src/components/Footer.tsx
@@ -1,18 +1,8 @@
-import { Container, Row, Col } from 'react-bootstrap';
-
-function Footer() {
+export default function Footer() {
   const currentYear = new Date().getFullYear();
   return (
-    <footer>
-      <Container>
-        <Row>
-          <Col className="text-center py-3">
-            <p>HouseFinder &copy; {currentYear}</p>
-          </Col>
-        </Row>
-      </Container>
+    <footer className="flex p-4 text-center">
+      HouseFinder &copy; {currentYear}
     </footer>
   );
 }
-
-export default Footer;
