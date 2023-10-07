942e2220b32bc3277ada537b45b697f032e1847b
Yzma <10090913+Yzma@users.noreply.github.com>
2023-10-07 00:29:32 -0400

    Added mobile view using Sheet component for Header

--- frontendV2/src/components/Header/Header.tsx
+++ frontendV2/src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import logo from '@assets/images/HouseLogoGrey.svg';
 import { useSelector } from 'react-redux';
-import { LogOut, User, UserCircle } from 'lucide-react';
+import { LogOut, User, UserCircle, Menu } from 'lucide-react';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -10,6 +10,16 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@components/ui/dropdown';
+
+import { Cross2Icon } from '@radix-ui/react-icons';
+import {
+  Sheet,
+  SheetClose,
+  SheetOverlay,
+  SheetPortal,
+  SheetPrimitiveContent,
+  SheetTrigger,
+} from '@/components/ui/sheet';
 
 export default function Header() {
   const { userInfo } = useSelector((state) => state.auth);
@@ -25,7 +35,32 @@ export default function Header() {
             <img width={40} src={logo} alt="HouseFinder" />
             <p>House-Tracker</p>
           </a>
-          <div className="flex items-center gap-x-4 font-bold text-gray-700">
+
+          <div className="lg:hidden">
+            <Sheet>
+              <SheetTrigger asChild>
+                <Menu className="h-6 w-6 hover:text-gray-500" />
+              </SheetTrigger>
+              <SheetPortal>
+                <SheetOverlay />
+                <SheetPrimitiveContent
+                  side="top"
+                  className="h-screen data-[state=open]:duration-300"
+                >
+                  <ol className="pt-12 flex flex-col items-end pr-8 space-y-4 font-bold text-2xl">
+                    <a href="/">Profile</a>
+                    <a href="/">Logout</a>
+                  </ol>
+                  <SheetClose className="absolute right-9 top-8 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-0 disabled:pointer-events-none data-[state=open]:bg-secondary">
+                    <Cross2Icon className="h-6 w-6" />
+                    <span className="sr-only">Close</span>
+                  </SheetClose>
+                </SheetPrimitiveContent>
+              </SheetPortal>
+            </Sheet>
+          </div>
+
+          <div className="hidden lg:flex lg:mx-auto lg:items-center lg:w-auto lg:space-x-6">
             <a className="hover:text-gray-900" href="/faq">
               FAQ
             </a>
