b0c6324c069d4ca6a074e668b5831875167a2947
javascriptsucks <evix1988@live.cn>
2023-10-06 18:37:20 -0600

    change file from jsx to tsx in ContextWrapper

--- frontendV2/src/ContextWrapper.tsx
+++ frontendV2/src/ContextWrapper.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import { ReactNode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 import store from './app/store';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function ContextWrapper({ children }) {
+interface ContextWrapperProps {
+  children: ReactNode;
+}
+
+export default function ContextWrapper({ children }: ContextWrapperProps) {
   return (
     <Provider store={store}>
       <BrowserRouter>{children}</BrowserRouter>
