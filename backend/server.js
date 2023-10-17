151f3484ae33ae95cbecaa91fd04782f945381b1
Adam Harvey <104089024+adamgrharvey@users.noreply.github.com>
2023-10-17 14:13:15 -0700

    Adam H Jest Testing v2 (#27)
    * First draft of User API testing. Adds required packages and test files
    
    * Adds tsconfig.json file
    
    * Config adjustments
    
    * Fix CORS dependency issue
    
    * Remove console.log
    
    * Adds URI for future testing DB.
    
    * Migrates express app to its own file, so that it can be used by Jest without listen. Adds more testing.
    
    * Organizes Testing variables

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,6 @@
-import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/database.js';
-import houseRouter from './routers/houseRouter.js';
-import userRouter from './routers/userRouter.js';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
+import app from './app.js';
 
 dotenv.config();
 
@@ -12,22 +8,5 @@ connectDB();
 
 const port = process.env.PORT || 5000;
 
-const app = express();
-
-const corsOptions = {
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173/'],
-  credentials: true,
-  methods: 'GET,POST,PUT,DELETE',
-  allowedHeaders: 'Content-Type, Authorization',
-};
-
-app.use(cors(corsOptions));
-
-app.use(express.json());
-
-app.use(cookieParser());
-
-app.use('/api/houses', houseRouter);
-app.use('/api/users', userRouter);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
