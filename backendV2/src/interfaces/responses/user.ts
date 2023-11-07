b14a27bd5f8368e660dfc52ed522bd20feaddd99
javascriptsucks <evix1988@live.cn>
2023-11-06 22:38:26 -0600

    created googleLogin response method in UserController

--- backendV2/src/interfaces/responses/user.ts
+++ backendV2/src/interfaces/responses/user.ts
@@ -7,9 +7,14 @@ export interface LoginUserData {
   role: string;
 }
 
+export interface GoogleLogin {
+  url: string;
+}
+
 export interface RegisterUserData extends LoginUserData {}
 
 export type RegisterUserResponse = GeneralResponse<RegisterUserData>;
 export type LoginUserResponse = GeneralResponse<LoginUserData>;
 export type GetUserResponse = GeneralResponse<IUser>;
 export type GetUsersResponse = GeneralResponse<IUser[]>;
+export type GoogleLoginResponse = GeneralResponse<GoogleLogin>;
