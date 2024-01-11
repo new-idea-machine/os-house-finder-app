0c9ced35ef6178aaba5e7919f700ec7dbaab5f8e
jmeng-sys <charlesmeng268@gmail.com>
2024-01-10 18:46:46 -0700

    calculation of house score

--- backendV2/src/controllers/houseScoreController.ts
+++ backendV2/src/controllers/houseScoreController.ts
@@ -3,23 +3,46 @@ import { StatusCodes } from '../constant';
 
 export const createHouseScore = (req: Request, res: Response) => {
   try {
-    const { squareFootScore } = req.body;
+    const { squareFootage, maxSquareFootage, minSquareFootage } = req.body;
     const { squareFootWeight } = req.body;
-    const { bedRoomScore } = req.body;
+    const { reqBedrooms, actualBedrooms, ensuites, powderRooms } = req.body;
     const { bedRoomWeight } = req.body;
-    const { travelScore } = req.body;
+    const { avgTravelTime, maxTravelTime } = req.body;
     const { travelWeight } = req.body;
-    const { lotSizeScore } = req.body;
+    const { storieNumber, depth, frontage } = req.body;
     const { lotSizeWeight } = req.body;
-    const { bathroomScore } = req.body;
+    const { reqBathrooms, actualBathrooms } = req.body;
     const { bathroomWeight } = req.body;
 
-    const score =
-      squareFootScore * squareFootWeight +
-      bedRoomScore * bedRoomWeight +
-      travelScore * travelWeight +
-      lotSizeScore * lotSizeWeight +
-      bathroomScore * bathroomWeight;
+    const squareFootScore =
+      2 * (squareFootage - minSquareFootage) -
+      (1 / (maxSquareFootage - minSquareFootage)) *
+        (squareFootage - minSquareFootage) ** 2;
+
+    const bedRoomScore =
+      (Math.atan((reqBedrooms + 1) * actualBedrooms) * 100) / (reqBedrooms + 1);
+
+    const bathroomBonus = ensuites + powderRooms / 2;
+    const bathroomScore =
+      (Math.atan((reqBathrooms + 1) * (actualBathrooms + bathroomBonus)) *
+        100) /
+      (reqBathrooms + 1);
+
+    const travelScore = Math.max((50 / avgTravelTime) * maxTravelTime + 100, 0);
+
+    const houseFootprint = squareFootage / storieNumber;
+    const landFootprint = depth * frontage * 3.28084;
+    const lotSizeScore = (1 - houseFootprint / landFootprint) * 100;
+
+    const score = Number(
+      (
+        squareFootScore * squareFootWeight +
+        bedRoomScore * bedRoomWeight +
+        travelScore * travelWeight +
+        lotSizeScore * lotSizeWeight +
+        bathroomScore * bathroomWeight
+      ).toFixed(2)
+    );
 
     res.status(StatusCodes.OK).json({ score });
   } catch (error) {
